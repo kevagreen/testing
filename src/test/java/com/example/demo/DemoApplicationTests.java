b480edb9e9f3e85abf59b87bf20f148915a49ffa
kevagreen <kevareynolds@gmail.com>
2021-04-01 07:56:19 -0600

    first commit

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -3,11 +3,31 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+
 class DemoApplicationTests {
 
+	Calculator underTest = new Calculator();
+
 	@Test
-	void contextLoads() {
+	void itShouldAddTwoNumbers() {
+		//given
+		int numbOne = 20;
+		int numTwo = 30;
+
+		//when
+		int result = underTest.add(numbOne, numTwo);
+
+		//then
+		int expected = 50;
+		assertThat(result).isEqualTo(expected);
+	}
+
+	class Calculator{
+		int add(int a, int b){
+			return a + b;
+		}
 	}
 
 }
---
+++ src/test/java/com/example/demo/student/StudentServiceTest.java
@@ -0,0 +1,48 @@
+package com.example.demo.student;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class StudentServiceTest {
+
+    @Mock
+    private StudentRepository studentRepository;
+    private StudentService underTest;
+
+    @BeforeEach
+    void setUp(){
+
+        underTest = new StudentService(studentRepository);
+    }
+    @AfterEach
+    void tearDown() throws Exception{
+
+    }
+
+    @Test
+    void canGetAllStudents() {
+        //when
+        underTest.getAllStudents();
+        //then
+        verify(studentRepository).findAll();
+    }
+
+    @Test
+    @Disabled
+    void addStudent() {
+    }
+
+    @Test
+    @Disabled
+    void deleteStudent() {
+    }
+}
